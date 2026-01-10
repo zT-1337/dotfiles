1c3d4b660e3435594f17d1d3b2d0d8356bdae75b
Thomas Zerr <thomas.zerr@gmail.com>
2026-01-10 21:33:18 +0100

    Add Astro Community Packs for HTML, CSS, JS and TS

--- nvim/lua/plugins/astrocommunity.lua
+++ nvim/lua/plugins/astrocommunity.lua
@@ -1,5 +1,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.cpp" }
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" }
 }
